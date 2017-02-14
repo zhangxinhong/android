ede35c0cc51221ec3ac55d4b487f8abd7592846e
zhangxinhong <zhangxinhongde163@163.com>
2017-02-14 14:44:55 +0800

    修改过了

--- TestGit/src/com/example/testgit/MainActivity.java
+++ TestGit/src/com/example/testgit/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.testgit;
 
 import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+@SuppressLint("NewApi")
 public class MainActivity extends ActionBarActivity {
 
 	@Override
